34dd1179c4fd3229a72d661b92184ade2f7d1cb7
diackomotamedi242-cpu <diackomotamedi242@gmail.com>
2026-07-18 19:13:24 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  // اضافه کردن این خط برای گیت‌هاب پیجز حیاتی است
+  // 
   base: "/DiaPastash/", 
   
   plugins: [react(), tailwindcss(), viteSingleFile()],
